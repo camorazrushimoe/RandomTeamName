8373cbd95e5a886366efeba86b3f9ba15d1376c6
Sergey <biginf.com@gmail.com>
2017-10-19 17:56:56 +0300

    add home page

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"math/rand"
 	"time"
 
@@ -10,6 +11,18 @@ import (
 
 	"github.com/gorilla/mux"
 )
+
+// GetMainPage show templates/index.html page
+func GetMainPage(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	//fmt.Println(posts)
+
+	t.ExecuteTemplate(w, "index", nil)
+}
 
 // GetTeamName function bla
 func GetTeamName(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +40,7 @@ func GetTeamName(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/", GetTeamName).Methods("GET")
+	router.HandleFunc("/", GetMainPage).Methods("GET")
+	router.HandleFunc("/random-name", GetTeamName).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
